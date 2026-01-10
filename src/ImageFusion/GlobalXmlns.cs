4f6eb14bc0737a329b77fbc30962ed18225a03f4
Hoàng Vũ <lop94.nguyenhoangvu@gmail.com>
2026-01-10 20:21:49 +0700

    define project

---
+++ src/ImageFusion/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ImageFusion")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ImageFusion.Pages")]
